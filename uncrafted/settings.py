7fb63249cd2ef0562b4bfb71d626920552f869e1
Shalane Proctor <shalane088@gmail.com>
2023-01-10 10:36:37 -0600

    corrected issues preventing manage.py

--- uncrafted/settings.py
+++ uncrafted/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 from pathlib import Path
 from decouple import config
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / ‘subdir’.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -29,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+# UPDATE THIS
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,11 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'uncraftedapi',
 ]
 
+# THIS IS NEW
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+)
+
+# UPDATE THIS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ uncraftedapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UncraftedapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uncraftedapi'
